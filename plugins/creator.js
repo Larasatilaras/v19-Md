6f69f647df05365836ffddebbf4dfe4f54cfd9b5
Laras <96417338+Larasatilaras@users.noreply.github.com>
2022-02-10 04:05:19 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -4,8 +4,8 @@ async function handler(m) {
                     + 'VERSION:3.0\n' 
                     + 'N:;KingOfBear;;;'
                     + 'FN:KingOfBear\n' // full name
-                    + 'ORG:Owner YukiBot;\n' // the organization of the contact
-                    + 'TEL;type=CELL;type=VOICE;waid=6288279268363:+6288279268363\n' // WhatsApp ID + phone number
+                    + 'ORG:Owner LarasBotz;\n' // the organization of the contact
+                    + 'TEL;type=CELL;type=VOICE;waid=6285759662798:+6285759662898\n' // WhatsApp ID + phone number
                     + 'END:VCARD'
                 conn.sendMessage(m.chat, { contacts: { displayName: 'King Of Bear', contacts: [{ vcard }] } }, { quoted: m })
 }
